1e1f377c9f8a5f0622cc10ad97ac2002a8baaba5
Joshua Rohan-Gould <z5161697@ad.unsw.edu.au>
2019-10-30 00:13:53 +1100

    Flesh out Pickup Abstract Class

--- src/dungeon/backend/entity/Pickup.java
+++ src/dungeon/backend/entity/Pickup.java
@@ -1,8 +1,16 @@
 package dungeon.backend.entity;
 
-
-public interface Pickup {
-
-    public void useItem(Player p);
+import dungeon.backend.PickUpBehaviour.*;
+public abstract class Pickup extends Entity {
+	
+	private PickUpBehaviour pickupBehaviour;
+	
+	public Pickup(int x, int y, PickUpBehaviour pickupBehaviour) {
+		super(x,y);
+		this.pickupBehaviour = pickupBehaviour;
+	}
+	
+    public void useItem(Player p) {
+	}
 
 }
