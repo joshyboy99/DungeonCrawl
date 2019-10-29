470f548298316257b203a6dda7456d4afc4b47db
z5260651 <z5260651@cse.unsw.edu.au>
2019-10-30 01:33:01 +1100

    Started boulder and movable

--- src/dungeon/backend/ContactBehaviour/Moveable.java
+++ src/dungeon/backend/ContactBehaviour/Moveable.java
@@ -11,6 +11,21 @@ public class Moveable implements ContactBehaviour {
 	public void onTouch(Entity e) {
 		int playerPositionX= e.getX();
 		int playerPositionY = e.getY();
+		//if behind obj
+		if(playerPositionX - e.getX() <1) {
+			
+		}  //if to the left of obj
+		else if() {
+			
+		} //if to the right of obj
+		else if() {
+			
+		} else { //if up top of obj 
+			
+		}
+		
+		
+		
 		
 	}
 
