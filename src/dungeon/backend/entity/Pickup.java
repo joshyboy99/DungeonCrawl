3824e81e799bdb12269b77c3bc8272bad5df66a3
Joshua Rohan-Gould <z5161697@ad.unsw.edu.au>
2019-10-30 00:30:38 +1100

    Pickup Constructor

--- src/dungeon/backend/entity/Pickup.java
+++ src/dungeon/backend/entity/Pickup.java
@@ -7,8 +7,10 @@ public abstract class Pickup extends Entity {
 	
 	private PickUpBehaviour pickupBehaviour;
 	
-	public Pickup(int x, int y,MoveBehaviour moveBehaviour, ContactBehaviour contactBehaviour, PickUpBehaviour pickupBehaviour) {
-		super(x,y, moveBehaviour, contactBehaviour);
+	public Pickup(int x, int y,MoveBehaviour moveBehaviour, PickUpBehaviour pickupBehaviour) {
+		super(x,y);
+		this.moveBehaviour = moveBehaviour;
+		this.contactBehaviour = new Canpickup(this);
 		this.pickupBehaviour = pickupBehaviour;
 	}
 	
