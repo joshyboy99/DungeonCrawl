f3c997ff5dbd49b533e3d7c123cb7c6aeb2d81c0
Joshua Rohan-Gould <z5161697@ad.unsw.edu.au>
2019-10-30 00:27:27 +1100

    fic pickup constructor

--- src/dungeon/backend/entity/Pickup.java
+++ src/dungeon/backend/entity/Pickup.java
@@ -1,16 +1,21 @@
 package dungeon.backend.entity;
 
 import dungeon.backend.PickUpBehaviour.*;
+import dungeon.backend.ContactBehaviour.*;
+import dungeon.backend.MoveBehaviour.*;
 public abstract class Pickup extends Entity {
 	
 	private PickUpBehaviour pickupBehaviour;
 	
-	public Pickup(int x, int y, PickUpBehaviour pickupBehaviour) {
-		super(x,y);
+	public Pickup(int x, int y,MoveBehaviour moveBehaviour, ContactBehaviour contactBehaviour, PickUpBehaviour pickupBehaviour) {
+		super(x,y, moveBehaviour, contactBehaviour);
 		this.pickupBehaviour = pickupBehaviour;
 	}
 	
     public void useItem(Player p) {
+    	
 	}
+    
+    public 
 
 }
