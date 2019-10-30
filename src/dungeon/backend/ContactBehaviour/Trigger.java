c47f3cade7f78868483b342b3691dea014c7e718
Joshua Rohan-Gould <z5161697@ad.unsw.edu.au>
2019-10-31 00:32:31 +1100

    Implementing Floor-Switch and Observer Pattern

--- src/dungeon/backend/ContactBehaviour/Trigger.java
+++ src/dungeon/backend/ContactBehaviour/Trigger.java
@@ -5,12 +5,20 @@ import dungeon.backend.entity.Entity;
 
 public class Trigger extends InteractableBehaviour implements ContactBehaviour {
 
+	boolean triggered;
+	
 	public Trigger(Entity attached) {
 		super(attached);
+		triggered = false;
 	}
 	
 	@Override
 	public void onTouch(Entity entity) {
+		
+		if(checkEntity(entity)) {
+			triggered = ;
+		}
+		
 	}
 	
 	@Override
--- src/dungeon/backend/entity/FloorSwitch.java
+++ src/dungeon/backend/entity/FloorSwitch.java
@@ -1,10 +1,33 @@
 package dungeon.backend.entity;
 
-public class FloorSwitch extends Entity {
+import dungeon.backend.ContactBehaviour.*;
+import dungeon.backend.MoveBehaviour.*;
 
+public class FloorSwitch extends Entity implements Observable{
+
+	String State;
+	
 	public FloorSwitch(int x, int y) {
-		super(x, y);
-		// TODO Auto-generated constructor stub
+		super(x, y, new Static(), null);
+		this.contactBehaviour = new Trigger(this);
+		State = "Open";
 	}
+
+	public void checkState() {
+		
+	}
+	
+	public boolean isClosed() {
+		if(State.equals("Open")) {
+			return true;
+		}
+		
+		else return false;
+	}
+	
+	public void changeState(){
+		
+	}
+	
 
 }
