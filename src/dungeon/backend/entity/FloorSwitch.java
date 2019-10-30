76554bdca42a6a2dbf0ea12219173750d1bfa849
Joshua Rohan-Gould <z5161697@ad.unsw.edu.au>
2019-10-31 00:57:47 +1100

    WIP Observer + Observable. WIP Trigger and Floor-Switch

--- src/dungeon/backend/entity/FloorSwitch.java
+++ src/dungeon/backend/entity/FloorSwitch.java
@@ -1,16 +1,21 @@
 package dungeon.backend.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import dungeon.backend.ContactBehaviour.*;
 import dungeon.backend.MoveBehaviour.*;
 
 public class FloorSwitch extends Entity implements Observable{
 
 	String State;
+	List<Observer> Observers;
 	
 	public FloorSwitch(int x, int y) {
 		super(x, y, new Static(), null);
 		this.contactBehaviour = new Trigger(this);
 		State = "Open";
+		Observers = new ArrayList<Observer>();
 	}
 
 	public void checkState() {
@@ -26,6 +31,26 @@ public class FloorSwitch extends Entity implements Observable{
 	}
 	
 	public void changeState(){
+		if(contactBehaviour.)
+	}
+
+	@Override
+	public void notifyObservers() {
+		for (Observer o : Observers) {
+			o.update(this.isClosed());
+		}
+		
+	}
+
+	@Override
+	public void addObserver(Observer o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		// TODO Auto-generated method stub
 		
 	}
 	
--- src/dungeon/backend/entity/Observer.java
+++ src/dungeon/backend/entity/Observer.java
@@ -3,4 +3,8 @@ package dungeon.backend.entity;
 public interface Observer {
 
 	public void update(Player player);
+
+	public void update(boolean bool);
+	
+	
 }
