6eb31fa051fa3aade33a00e76836963ddb75787b
z5260651 <z5260651@cse.unsw.edu.au>
2019-10-30 11:46:27 +1100

    Finished prelim moveable

--- src/dungeon/backend/ContactBehaviour/Moveable.java
+++ src/dungeon/backend/ContactBehaviour/Moveable.java
@@ -13,17 +13,17 @@ public class Moveable extends InteractableBehaviour implements ContactBehaviour 
 	public void onTouch(Entity e) {
 		int entityPositionX= e.getX();
 		int entityPositionY = e.getY();
-		//if behind obj
-		if(playerPositionX - e.getX() < 1 && ) {
-			
-		}  //if to the left of obj
-		else if() {
-			
+		//if to left of obj
+		if(attached.getX() - entityPositionX == 1) {
+			attached.setX(attached.getX()+1);
+		}  //if bottom of obj
+		else if(attached.getY() - entityPositionY == -1) {
+			attached.setY(attached.getY()-1);
 		} //if to the right of obj
-		else if() {
-			
+		else if(attached.getX() - entityPositionX == -1) {
+			attached.setX(attached.getX()-1);
 		} else { //if up top of obj 
-			
+			attached.setY(attached.getY()+1);
 		}
 		
 		
