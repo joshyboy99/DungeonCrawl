601078d299c8b6e9435d98155541fccdd3515c8d
z5260651 <z5260651@cse.unsw.edu.au>
2019-10-31 20:39:08 +1100

    changed enemy constructor

--- src/dungeon/backend/entity/Enemy.java
+++ src/dungeon/backend/entity/Enemy.java
@@ -1,5 +1,6 @@
 package dungeon.backend.entity;
 
+import dungeon.backend.Dungeon;
 import dungeon.backend.ContactBehaviour.*;
 import dungeon.backend.MoveBehaviour.*;
 
@@ -7,12 +8,13 @@ public class Enemy extends Entity {
 	
 
 	private Player player;
-	
-	public Enemy(int x, int y, Player player) {
+	private Dungeon dungeon;
+	public Enemy(int x, int y, Player player, Dungeon dungeon) {
 		super(x, y);
 		this.player = player;
 		this.contactBehaviour = new Destroy(this);
-		this.moveBehaviour = new MoveTowards();
+		this.moveBehaviour = new MoveTowards(this, player, dungeon);
+		this.dungeon = dungeon;
 	}
 	
 	
