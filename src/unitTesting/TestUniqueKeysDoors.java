5e16e5f1ff774b73767ea81c5c37026abc0cf541
Amy <doramyy@gmail.com>
2019-11-03 01:36:38 +1100

    Test unique keys

--- src/unitTesting/TestUniqueKeysDoors.java
+++ src/unitTesting/TestUniqueKeysDoors.java
@@ -2,7 +2,11 @@ package unitTesting;
 
 import dungeon.backend.entity.*;
 import dungeon.backend.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 /* Combination of :
@@ -23,6 +27,40 @@ class TestUniqueKeysDoors {
         // check that unused door is not open (locked)
         // unlock door with corresponding key
         // check that the door is now open
+        Dungeon dungeon = new Dungeon(5,1);
+
+        Player player = new Player(dungeon, 1 ,1);
+        Key key = new Key(0, 1, 1, dungeon);
+        dungeon.addEntity(key);
+        Door door = new Door(2,1,1,dungeon);
+        dungeon.addEntity(door);
+        
+        Inventory invent = player.getInventory();
+        
+        System.out.println("- -    TEST OPEN DOOR   - -");
+        
+        player.moveRight();
+        assertTrue(player.x().getValue().intValue() == 1);
+        assertTrue(player.y().getValue().intValue() == 1);
+        
+        player.moveLeft();
+        player.activePickup();
+        assertTrue(player.checkInventory(key));
+        assertFalse(dungeon.checkEntitiesOnDungeon(key));
+        
+        player.moveRight();
+        player.moveRight();
+        player.moveRight();
+        player.moveRight();
+        player.moveRight();
+        System.out.println(player.x().getValue().intValue());
+        System.out.println(player.y().getValue().intValue());
+//        assertTrue(player.x().getValue().intValue() == #);
+//        assertTrue(player.y().getValue().intValue() == #);
+        
+        
+        System.out.println("- -     TEST PASSED    - -");
+        
     }
     
     @Test
