3bb319daeb18cc898b31b80a9b4b089411f5ed2a
Amy <doramyy@gmail.com>
2019-11-03 02:38:06 +1100

    tried to debug composite levels

--- src/unitTesting/Test21CompositeLevels.java
+++ src/unitTesting/Test21CompositeLevels.java
@@ -2,7 +2,12 @@ package unitTesting;
 
 import dungeon.backend.entity.*;
 import dungeon.backend.*;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 /*
@@ -11,30 +16,75 @@ import org.junit.jupiter.api.Test;
  */
 
 class Test21CompositeLevels {
-	@Test
-	void testKillEnemiesAndExit() {
-        System.out.println("- - Test WIN COMPOSITE_1 - -");
-          
-        System.out.println("- -        PASSED        - -");
-		
-	}
+//	@Test
+//	void testKillEnemiesAndExit() {
+//        System.out.println("- - Test WIN COMPOSITE_1 - -");
+//          
+//        System.out.println("- -        PASSED        - -");
+//		
+//	}
 	
 	@Test
 	void testTreasuresAndFloorSwitch() {
 
         System.out.println("- - Test WIN COMPOSITE_2 - -");
+        Dungeon dungeon = new Dungeon(5, 5);
+		Player player = new Player(dungeon, 3, 2);
+		dungeon.setPlayer(player);
+		JSONObject goal = new JSONObject();
+		goal.put("goal", "AND");
+		
+		JSONArray subGoal = new JSONArray();
+		JSONObject sub1 = new JSONObject();
+		sub1.put("goal", "exit");
+		JSONObject sub2 = new JSONObject();
+		sub2.put("goal", "treasure");
+		
+		subGoal.put(sub1);
+		subGoal.put(sub2);
+		goal.put("subgoals", subGoal);
+		
+		dungeon.setupGoal(goal);
+		
+		Treasure treasure = new Treasure(4,2, dungeon);
+		dungeon.addEntity(treasure);
+		
+		Boulder boulder = new Boulder(2, 2, dungeon);
+		FloorSwitch fswitch = new FloorSwitch(1, 2, dungeon);
+		dungeon.addEntity(boulder);
+		dungeon.addEntity(fswitch);		
+		assertFalse(fswitch.getState());
+        System.out.println("- -        PASSED        - -");
+
+
+		assertFalse(dungeon.isComplete());
+        System.out.println("- -        PASSED        - -");
+
+
+		player.moveLeft();
+		dungeon.updateDungeon();
+		assertTrue(fswitch.getState());
+        System.out.println("- -        PASSED        - -");
+
+		assertFalse(dungeon.isComplete());
+		player.moveRight();
+		dungeon.updateDungeon();
+		assertFalse(dungeon.isComplete());
+		player.moveRight();
+		dungeon.updateDungeon();
+		assertTrue(dungeon.isComplete());
         
         System.out.println("- -        PASSED        - -");
 		
 	}
 	
-	@Test
-	void testTreasuresOrKillEnmiesAndExit() {
-		
-        System.out.println("- - Test WIN COMPOSITE_3 - -");
-        
-        System.out.println("- -        PASSED        - -");
-		
-	}
+//	@Test
+//	void testTreasuresOrKillEnmiesAndExit() {
+//		
+//        System.out.println("- - Test WIN COMPOSITE_3 - -");
+//        
+//        System.out.println("- -        PASSED        - -");
+//		
+//	}
 	
 }
