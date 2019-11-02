d1d89f508194be876b8eb7806575ebb1e6d4a587
z5260651 <z5260651@cse.unsw.edu.au>
2019-11-03 02:17:19 +1100

    Fixed locked allowed attributed

--- src/dungeon/backend/ContactBehaviour/Locked.java
+++ src/dungeon/backend/ContactBehaviour/Locked.java
@@ -9,16 +9,23 @@ public class Locked extends InteractableBehaviour implements ContactBehaviour {
 
 	public Locked(Entity attached) {
 		super(attached);
+		this.ValidEntities.add(new Player());
 	}	
 	
 	@Override
 	public void onTouch(Entity entity) {
+		if(checkValidEntityClass(entity)) {
 		// TODO Auto-generated method stub
 		//player cannot go through door if locked!!!!
 		entity.setMx(0);
 		entity.setMy(0);
 		//see if they have the right stuff, possibly changing the contact behaviour from locked.
 		attached.wasTouched(entity);
+		} else {
+			entity.setMx(0);
+			entity.setMy(0);
+		}
+		
 	}
 
 }
