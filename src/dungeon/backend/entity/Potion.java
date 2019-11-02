b77e1830c06ce9027ee174b8c307c4bc40bfd4fd
Joshua Rohan-Gould <z5161697@ad.unsw.edu.au>
2019-11-02 16:10:58 +1100

    Implementing Timer Using Duration

--- src/dungeon/backend/entity/Potion.java
+++ src/dungeon/backend/entity/Potion.java
@@ -1,5 +1,7 @@
 package dungeon.backend.entity;
 
+import java.time.Duration;
+
 import Effects.*;
 import dungeon.backend.ContactBehaviour.*;
 import dungeon.backend.MoveBehaviour.*;
@@ -7,7 +9,8 @@ import dungeon.backend.PickUpBehaviour.*;
 
 public class Potion extends Pickup implements Observer{
 	
-	private int Timer  
+	private Duration Timer;  
+	
 	
 	public Potion(int x, int y) {
 		super(x, y);
@@ -15,8 +18,16 @@ public class Potion extends Pickup implements Observer{
 		this.contactBehaviour = new ActivePickup(this);
 		this.pickupBehaviour = new AffectPicker(this);
 		this.effects.add(new InstaKillEnemy(this));
+		this.Timer = 90;
 	}
-
+	
+	private void PotionTimer() {
+		while(int i = Timer; i > 0; i -  )
+		for (Effect e: effects) {
+			e.endEffect(e);
+		}
+	}
+	
 	@Override
 	public void update(Player player) {
 		// TODO Auto-generated method stub
