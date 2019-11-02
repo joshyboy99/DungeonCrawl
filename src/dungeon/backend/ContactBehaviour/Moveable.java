dcd4e75bb0eb86f2a70fa87b5a61ef45cb9728fb
z5260651 <z5260651@cse.unsw.edu.au>
2019-11-02 21:20:34 +1100

    Added player to list of allowed entities for boulder

--- src/dungeon/backend/ContactBehaviour/Moveable.java
+++ src/dungeon/backend/ContactBehaviour/Moveable.java
@@ -8,53 +8,55 @@ public class Moveable extends InteractableBehaviour implements ContactBehaviour 
  
 	public Moveable(Entity attached) {
 		super(attached);
+		this.ValidEntities.add(new Player(-1,-1));	
 	}
 	@Override
 	public void onTouch(Entity e) {
-		int entityPositionX= e.getX();
-		int entityPositionY = e.getY();
-		//if to left of obj
-		if(attached.getX() - entityPositionX == 1 && attached.getX() < attached.getDungeonWidth() - 1) {
-			attached.setMx(1);
-			attached.scanDungeonTile(attached.getX() + 1, attached.getY());
-			//if attached has failed to move, then obviously entity cannot either!
-			if(attached.getMx() == 0) {
+		if(checkValidEntityClass(e)) {
+			int entityPositionX= e.getX();
+			int entityPositionY = e.getY();
+			//if to left of obj
+			if(attached.getX() - entityPositionX == 1 && attached.getX() < attached.getDungeonWidth() - 1) {
+				attached.setMx(1);
+				attached.scanDungeonTile(attached.getX() + 1, attached.getY());
+				//if attached has failed to move, then obviously entity cannot either!
+				if(attached.getMx() == 0) {
+					e.setMx(0);
+				}
+				attached.setX(attached.getX()+ attached.getMx());
+			}  //if bottom of obj
+			else if(attached.getY() - entityPositionY == -1 && attached.getY() > 0) {
+				attached.setMy(-1);
+				attached.scanDungeonTile(attached.getX(), attached.getY() - 1);
+				//if attached has failed to move, then obviously entity cannot either!
+				if(attached.getMy() == 0) {
+					e.setMy(0);
+				}
+				attached.setY(attached.getY()+ attached.getMy());
+			} //if to the right of obj
+			else if(attached.getX() - entityPositionX == -1 && attached.getX() > 0) {
+				attached.setMx(-1);
+				attached.scanDungeonTile(attached.getX() - 1, attached.getY());
+				//if attached has failed to move, then obviously entity cannot either!
+				if(attached.getMx() == 0) {
+					e.setMx(0);
+				}
+				attached.setX(attached.getX()+ attached.getMx());
+			} else if(attached.getY() - entityPositionY == 1 && attached.getY() < attached.getDungeonHeight() - 1){ //if up top of obj 
+				attached.setMy(1);
+				attached.scanDungeonTile(attached.getX(), attached.getY() + 1);
+				//if attached has failed to move, then obviously entity cannot either!
+				if(attached.getMy() == 0) {
+					e.setMy(0);
+				}
+				attached.setY(attached.getY()+ attached.getMy());
+			} else {
+				//oh no - attached is attempting to go out of bounds, stop entity from moving!!!!!
 				e.setMx(0);
-			}
-			attached.setX(attached.getX()+ attached.getMx());
-		}  //if bottom of obj
-		else if(attached.getY() - entityPositionY == -1 && attached.getY() > 0) {
-			attached.setMy(-1);
-			attached.scanDungeonTile(attached.getX(), attached.getY() - 1);
-			//if attached has failed to move, then obviously entity cannot either!
-			if(attached.getMy() == 0) {
 				e.setMy(0);
 			}
-			attached.setY(attached.getY()+ attached.getMy());
-		} //if to the right of obj
-		else if(attached.getX() - entityPositionX == -1 && attached.getX() > 0) {
-			attached.setMx(-1);
-			attached.scanDungeonTile(attached.getX() - 1, attached.getY());
-			//if attached has failed to move, then obviously entity cannot either!
-			if(attached.getMx() == 0) {
-				e.setMx(0);
-			}
-			attached.setX(attached.getX()+ attached.getMx());
-		} else if(attached.getY() - entityPositionY == 1 && attached.getY() < attached.getDungeonHeight() - 1){ //if up top of obj 
-			attached.setMy(1);
-			attached.scanDungeonTile(attached.getX(), attached.getY() + 1);
-			//if attached has failed to move, then obviously entity cannot either!
-			if(attached.getMy() == 0) {
-				e.setMy(0);
-			}
-			attached.setY(attached.getY()+ attached.getMy());
-		} else {
-			//oh no - attached is attempting to go out of bounds, stop entity from moving!!!!!
-			e.setMx(0);
-			e.setMy(0);
 		}
-	}
-
+   }
 }
 
 /* KNOWN ISSUES:
