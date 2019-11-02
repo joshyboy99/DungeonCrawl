44b219f1a3bcbd7424ff588fcb0283c9341eec14
z5260651 <z5260651@cse.unsw.edu.au>
2019-11-03 03:10:43 +1100

    added test01,02

--- src/dungeon/backend/entity/Player.java
+++ src/dungeon/backend/entity/Player.java
@@ -15,6 +15,7 @@ import dungeon.backend.ContactBehaviour.*;
  */
 public class Player extends Entity {
 
+	private String facing; 
     private int treasureScore;
     private Inventory inventory;
     private List<Observer> observers = new ArrayList<Observer>();
@@ -71,6 +72,7 @@ public class Player extends Entity {
     }
     
     public void moveUp() {
+    	this.facing = "UP";
         if (getY() > 0) {
         	this.setMy(-1);
         	dungeon.scanTile(this, getX(), getY() - 1);
@@ -79,6 +81,7 @@ public class Player extends Entity {
     }
 
     public void moveDown() {
+    	this.facing = "DOWN";
         if (getY() < dungeon.getHeight() - 1) {
         	this.setMy(1);
         	dungeon.scanTile(this, getX(), getY() + 1);
@@ -88,6 +91,7 @@ public class Player extends Entity {
     }
 
     public void moveLeft() {
+    	this.facing = "LEFT";
         if (getX() > 0 ) {
         	this.setMx(-1);
         	dungeon.scanTile(this, getX() -1 , getY());
@@ -96,6 +100,7 @@ public class Player extends Entity {
     }
 
     public void moveRight() {
+    	this.facing = "RIGHT";
         if (getX() < dungeon.getWidth() - 1){
         	this.setMx(1);
         	dungeon.scanTile(this, getX() + 1 , getY());
@@ -158,7 +163,28 @@ public class Player extends Entity {
     	return this.dungeon;
     }
     
+    public boolean hasSword() {
+    	for(Pickup p: this.getInventory().getItems()) {
+    		if(p instanceof Sword) {
+    			return true;
+    		}
+    	} 
+    	return false;
+    }
+    
+    public Sword getSword() {
+    	for(Pickup p: this.getInventory().getItems()) {
+    		if(p instanceof Sword) {
+    			return (Sword)p;
+    		}
+    	}
+    	return null; 
+    }
+    
     public void useSword() {
+    	if(this.hasSword()) {
+    		this.getSword().Swing(this.facing, this.getX(), this.getY());
+    	} 
     }
     
     public int getCurrentKeyID(){
