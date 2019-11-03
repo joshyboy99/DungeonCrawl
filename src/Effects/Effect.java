d5278850bdcc895de03e0539adb9d897944c07ea
Amy <doramyy@gmail.com>
2019-11-03 12:39:10 +1100

    javadoced all effect

--- src/Effects/Effect.java
+++ src/Effects/Effect.java
@@ -2,6 +2,11 @@ package Effects;
 
 import dungeon.backend.entity.*;
 
+/**
+ * Effects of the entity
+ * @author amyluo
+ *
+ */
 public interface Effect {
 	public void effect(Entity e);
 	public void endEffect();
