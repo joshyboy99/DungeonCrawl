5075054aa3ddfcd5b59f784f9f8d2b6d2f7dd1c6
z5260651 <z5260651@cse.unsw.edu.au>
2019-11-11 21:19:28 +1100

    Added javadoc for die class

--- src/dungeon/backend/ContactBehaviour/Die.java
+++ src/dungeon/backend/ContactBehaviour/Die.java
@@ -2,6 +2,18 @@ package dungeon.backend.ContactBehaviour;
 
 import dungeon.backend.InteractableBehaviour;
 import dungeon.backend.entity.*;
+
+
+/**
+ * This class is part of the group that Extends ContactBehaviour.
+ * If the player is touched by an enemy ( the only allowed class of this contact behaviour)
+ * , it will kill the player. 
+ * 
+ * 
+ * @author JAG
+ *
+ */
+
 public class Die extends InteractableBehaviour implements ContactBehaviour{
 
 	public Die(Entity attached) {
@@ -9,7 +21,9 @@ public class Die extends InteractableBehaviour implements ContactBehaviour{
 		this.ValidEntities.add(new Enemy());
 		// TODO Auto-generated constructor stub
 	}
-
+/**
+ * This onTouch will kill the attached as long as the entity that touches it is on the valid entities list. 
+ */
 	@Override
 	public void onTouch(Entity entity) {
 		if(this.checkValidEntityClass(entity)) {
