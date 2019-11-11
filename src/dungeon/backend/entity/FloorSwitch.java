7d145e63824eb3f12712c811f75555f8d469aaf3
Joshua Rohan-Gould <joshyboy99@gmail.com>
2019-11-11 23:31:46 +1100

    JavaDoc Exit and FloorSwitch

--- src/dungeon/backend/entity/FloorSwitch.java
+++ src/dungeon/backend/entity/FloorSwitch.java
@@ -6,8 +6,10 @@ import dungeon.backend.MoveBehaviour.*;
 
 
 /**
- * The FloorSwitch Entity Class
- * 
+ *  The Entity - FloorSwitch, is an inanimate obstacle that contains a boolean state 
+ *  which can be toggled by certain entities being on its coordinates. Pressing all 
+ *  floor switches is a possible goal for a dungeon.
+ *  
  * @author JAG
  *
  */
@@ -22,32 +24,46 @@ public class FloorSwitch extends Entity implements Observer {
 		this.state=false; 
 	}
 	
+	/**
+	 * This overridden wasTouched that toggles the switche's
+	 * state when called. 
+	 */
 	public void wasTouched() { 
-		if(this.state == true) {
-			this.state =false; 
-		} else {
-			this.state =true; 
-		}
+		this.state = !state;
 	}
 	
-
+	/**
+	 * Sets the State attribute of this switch to be 
+	 * the one that has been entered in the method.
+	 * @param state the desired state of the switch.
+	 */
 	public void setState(boolean state) {
 		this.state = state;
 	}
 	
+	/**
+	 * Gets the current state of the switch
+	 * @return a boolean value representing 
+	 * the boolean state of the switch. 
+	 * true = closed, false = open. 
+	 */
 	public boolean getState() {
 		return this.state;
 	}
 
+	/**
+	 * Determines whether the switched is closed or not.
+	 * @return true if closed, false otherwise.
+	 */
 	public boolean isClosed() {
-		if(this.state = true) {
-			return true;
-		}
-		
-		else return false;
+		return this.getState();
 	}
 
-
+	
+	/**
+	 * Inherited method for observer pattern.
+	 * Updates dungeon when state changes.
+	 */
 	@Override
 	public void update(Player player) {
 		
