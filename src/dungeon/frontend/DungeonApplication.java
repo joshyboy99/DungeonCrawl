563c77e3fc6567a71d5e8ff9c0055e238e4c9f78
Joshua Rohan-Gould <joshyboy99@gmail.com>
2019-11-13 00:33:51 +1100

    Merge Resolve

--- src/dungeon/frontend/DungeonApplication.java
+++ src/dungeon/frontend/DungeonApplication.java
@@ -11,28 +11,26 @@ public class DungeonApplication extends Application {
     
     @Override
     public void start(Stage primaryStage) throws IOException {
-//<<<<<<< HEAD
     	
-    	DungeonSelectScreen dungeonLevels = new DungeonSelectScreen(primaryStage);
-    	
-    	dungeonLevels.start();
-//=======
-//        primaryStage.setTitle("Dungeon");
-//
-//        DungeonControllerLoader dungeonLoader = new DungeonControllerLoader("advanced.json");
-//
-//        DungeonController controller = dungeonLoader.loadController();
-//
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("DungeonView.fxml"));
-//        loader.setController(controller);
-//        Parent root = loader.load();
-//        Scene scene = new Scene(root);
-//        root.requestFocus();
-//        
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//
-//>>>>>>> Cleanup
+//    	DungeonSelectScreen dungeonLevels = new DungeonSelectScreen(primaryStage);
+//    	
+//    	dungeonLevels.start();
+
+        primaryStage.setTitle("Dungeon");
+
+        DungeonControllerLoader dungeonLoader = new DungeonControllerLoader("advanced.json");
+
+        DungeonController controller = dungeonLoader.loadController();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("DungeonView.fxml"));
+        loader.setController(controller);
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        root.requestFocus();
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 
     public static void main(String[] args) {
