3fb9e233c2806168e9abf88672e777a8201ff572
Joshua Rohan-Gould <joshyboy99@gmail.com>
2019-11-15 22:56:03 +1100

    Begin fix for enemy kill

--- src/dungeon/backend/ContactBehaviour/Die.java
+++ src/dungeon/backend/ContactBehaviour/Die.java
@@ -19,8 +19,6 @@ public class Die extends InteractableBehaviour implements ContactBehaviour{
 	public Die(Entity attached) {
 		super(attached);
 		this.ValidEntities.add(new Enemy());
-		this.ValidEntities.add(new Player());
-		// TODO Auto-generated constructor stub
 	}
 	/**
 	 * This onTouch will kill the attached as long as the entity that touches it is on the valid entities list. 
